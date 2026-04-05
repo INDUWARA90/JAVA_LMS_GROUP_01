8e1d816ba471de9b338a0a8a2a770c98aba1562c
Dilmi Amandha <amandhadilmi@gmail.com>
2026-04-05 18:41:48 +0530

    update files

--- src/main/java/com/example/java_lms_group_01/model/Attendance.java
+++ src/main/java/com/example/java_lms_group_01/model/Attendance.java
@@ -1,13 +1,18 @@
 package com.example.java_lms_group_01.model;
 
 public class Attendance {
-    private int studentId;
-    private String subject;
-    private int totalSessions;
-    private int attendedSessions;
-    private int medicalSessions;
 
+    private int totalSessions;
+    private int attended;
+    private int medical;
+
+    // Calculate percentage
     public double getPercentage() {
-        return ((double)(attendedSessions + medicalSessions) / totalSessions) * 100;
+        return ((double)(attended + medical) / totalSessions) * 100;
+    }
+
+    // Check eligibility
+    public boolean isEligible() {
+        return getPercentage() >= 80;
     }
 }
--- src/main/java/com/example/java_lms_group_01/model/Medical.java
+++ src/main/java/com/example/java_lms_group_01/model/Medical.java
@@ -1,7 +1,10 @@
 package com.example.java_lms_group_01.model;
 
 public class Medical {
-    private int studentId;
-    private String date;
+
     private boolean approved;
+
+    public boolean isApproved() {
+        return approved;
+    }
 }
